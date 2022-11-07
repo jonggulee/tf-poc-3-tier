de04bfb9a709404b95a77d183d63bba3cbb8249b
Jonggu Lee <jg.jake.lee@mz.co.kr>
2022-11-07 11:08:19 +0900

    NAT Gateway 2개에서 1개로 변경

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,4 +1,9 @@
 # VPC
+output "vpc_id" {
+  description = "VPC에 할당한 CIDR block"
+  value       = aws_vpc.main.id
+}
+
 output "vpc_cidr_block" {
   description = "VPC에 할당한 CIDR block"
   value       = aws_vpc.main.cidr_block
@@ -18,6 +23,23 @@ output "private_subnets_cidr" {
 output "restricted_subnets_cidr" {
   description = "Restricted Subnet CIDR"
   value       = ["${aws_subnet.res.*.cidr_block}"]
+}
+
+# subnet ids
+
+output "public_subnets_ids" {
+  description = "Public Subnet ids"
+  value       = ["${aws_subnet.pub.*.id}"]
+}
+
+output "private_subnets_ids" {
+  description = "Private Subnet ids"
+  value       = ["${aws_subnet.pri.*.id}"]
+}
+
+output "restricted_subnets_ids" {
+  description = "Restricted Subnet ids"
+  value       = ["${aws_subnet.res.*.id}"]
 }
 
 # NAT 
